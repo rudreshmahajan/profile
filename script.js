fec9375d56cba779082724b716a7558ed1a8f4e4
rudreshmahajan <83824091+rudreshmahajan@users.noreply.github.com>
2021-10-17 14:05:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-		<script>
-			       function sendMail(params) {
+
+function sendMail(params) {
 
 var tempParams = {
 
 from_name: document.getElementById("fromName").value, 
 to_name: document.getElementById("toName").value,
 message: document.getElementById("msg").value,
+};
 
-emailjs.send('gmail', 'template_159r6l5",tempParams) 
-.then(function(res){
+emailjs.send('gmail', 'template_159r6l5',tempParams).then(function(res){
 console.log("success", res.status);
 })
 }
-					       
-</script>
+    
