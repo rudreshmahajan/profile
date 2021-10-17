9f57b173d941f0c41b7c4f3ab8266677c9818d1a
rudreshmahajan <83824091+rudreshmahajan@users.noreply.github.com>
2021-10-17 08:20:30 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -13,7 +13,7 @@ if (empty($name) ||empty($email) || empty($message))
 echo "  please fill the form";
   else
   {
-     mail("rahulbhaiya7788@gmail.com", "Webtech Message", $message ,"From: $name < $email>");
+     mail("projectno7777@gmail.com", "Webtech Message", $message ,"From: $name < $email>");
      echo "<script type='text/javascript'>alert('your message sent successfully');
      window.history.log(-1);
      </script>";
